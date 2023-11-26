f6e138d30e78c98d0ee2f86cac9766d789fd4c87
BioBabe2002 <150148922+BioBabe2002@users.noreply.github.com>
2023-11-26 20:38:24 +0000

    Update plot_data.R

--- plot_data.R
+++ plot_data.R
@@ -1,11 +1,11 @@
 #Script to plot the logistic growth data
 
-growth_data <- read.csv("???")
+growth_data <- read.csv("experiment2.csv")
 
 install.packages("ggplot2")
 library(ggplot2)
 
-ggplot(aes(t,N), data = ???) +
+ggplot(aes(t,N), data = growth_data) +
   
   geom_point() +
   
